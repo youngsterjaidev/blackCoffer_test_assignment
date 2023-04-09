a431a93b4aff8162340024009cc0dc7f9e75ff05
Jai Dev <47059807+youngsterjaidev@users.noreply.github.com>
2023-04-09 17:38:27 +0530

    Update hello.js

--- api/hello.js
+++ api/hello.js
@@ -1,9 +1,4 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
- 
-export default function handler(
-  request: VercelRequest,
-  response: VercelResponse,
-) {
+export default function handler(request, response) {
   response.status(200).json({
     body: request.body,
     query: request.query,
